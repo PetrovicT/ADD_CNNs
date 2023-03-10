75ccb05c1e3a4996b13e8d75160449c6209b7e8f
Teodora Petrovic <teodora.z.petrovic@gmail.com>
2023-03-10 02:21:34 +0100

    Added information about image collections

--- table_processing.py
+++ table_processing.py
@@ -2,7 +2,7 @@ import pandas as pd
 pd.set_option('display.max_rows', None)
 
 
-def upload_excel_number_of_subject(file_path, sheet_name, class_name):
+def upload_excel_number_of_subjects(file_path, sheet_name, class_name):
     xl_file = pd.ExcelFile(file_path)
     dfs = {sheet_name: xl_file.parse(sheet_name) for sheet_name in xl_file.sheet_names}
     df = dfs[sheet_name]
@@ -28,7 +28,7 @@ def upload_excel_number_of_subject(file_path, sheet_name, class_name):
 
 
 def split_string_find_image_id(image_name_string):
-    substrings = image_name.split("_")
+    substrings = image_name_string.split("_")
     image_id_length = len(substrings)
     full_image_id = substrings[image_id_length - 1]  # Iid
     # full_image_id[1::]  # only id, without letter I
@@ -59,9 +59,9 @@ def get_image_group(image_name_string, file_path, sheet_name):
 
 if __name__ == '__main__':
     path_file = './tables/preprocessed_OR.xlsx'
-    # upload_excel_number_of_subject(path_file, 'preprocessed_OR', "AD")
-    # upload_excel_number_of_subject(path_file, 'preprocessed_OR', "CN")
+    # upload_excel_number_of_subjects(path_file, 'preprocessed_OR', "AD")
+    # upload_excel_number_of_subjects(path_file, 'preprocessed_OR', "CN")
 
     image_name = "ADNI_002_S_0295_MR_MPR-R__GradWarp__B1_Correction__N3_Br_20070319114336780_S13407_I45112"
     image_group = get_image_group(image_name, path_file, 'preprocessed_OR')
-    print(image_group)
+    # print(image_group)
